8d1b0dacb4cc68a86616ae182688819d2b377c74
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-12-22 08:42:49 -0800

    add test plot test_fill.py

---
+++ test_plots/test_fill.py
@@ -0,0 +1,25 @@
+"""Plot to test text"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    fig, ax = plt.subplots()
+    ax.grid(color='lightgray')
+
+    x = np.linspace(0, 4 * np.pi, 1000)
+    y1 = 0.5 * np.sin(0.5 * x)
+    y2 = np.sin(x)
+    y3 = np.cos(x)
+
+    ax.fill(x, y1, alpha=0.3, facecolor='green')
+    ax.fill_between(x, y2, y3, alpha=0.3, facecolor='red')
+    ax.fill_between(x, -y2, -y3, alpha=0.3, facecolor='blue')
+
+    ax.set_xlim(0, 4 * np.pi)
+    ax.set_ylim(-1.1, 1.1)
+
+    return fig
+
+if __name__ == '__main__':
+    main()
+    plt.show()
