40f485d1a52e4028bef9a783c3460ed62760ac0c
John Eppley <jmeppley@gmail.com>
2013-12-21 22:22:46 -0500

    test module

---
+++ test_plots/test_markers.py
@@ -0,0 +1,24 @@
+"""Plot to test line styles"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    fig, ax = plt.subplots()
+
+    np.random.seed(0)
+    numPoints=10
+    for marker in ['or','vb','^g','+y','dw']:
+        ax.plot(np.random.normal(size=numPoints),
+                np.random.normal(size=numPoints),
+                marker, ms=5+20*np.random.uniform(), alpha=0.5*np.random.uniform())
+
+    ax.set_xlabel('this is x')
+    ax.set_ylabel('this is y')
+    ax.set_title('Marker test!', size=14)
+    ax.grid(color='lightgray', alpha=0.7)
+
+    return fig
+
+if __name__ == '__main__':
+    main()
+    plt.show()
