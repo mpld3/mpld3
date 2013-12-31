BUG: fix text rotation; add text artists

test_plots/test_contour.py
@@ -1,16 +1,12 @@
 """Plot to test line contours"""
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib
-import datetime
-import time
 import matplotlib.mlab as mlab
 
-def main():
 
-    delta = 0.025
-    x = np.arange(-3.0, 3.0, delta)
-    y = np.arange(-2.0, 2.0, delta)
+def main():
+    x = np.linspace(-3.0, 3.0, 30)
+    y = np.linspace(-2.0, 2.0, 30)
     X, Y = np.meshgrid(x, y)
     Z1 = mlab.bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
     Z2 = mlab.bivariate_normal(X, Y, 1.5, 0.5, 1, 1)