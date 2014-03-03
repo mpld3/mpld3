af1fc7d9986eeb00230c9a48c0fbb849ee4d83a7
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-03-03 06:34:58 -0800

    make mpld3 url point to mpld3.github.io

--- mpld3/urls.py
+++ mpld3/urls.py
@@ -7,8 +7,7 @@ __all__ = ["D3_URL", "MPLD3_URL", "D3_LOCAL", "MPLD3_LOCAL"]
 
 D3_URL = "http://d3js.org/d3.v3.min.js"
 
-MPLD3_URL = ("https://rawgithub.com/mpld3/mpld3_rewrite/master/"
-             "mpld3_rewrite/js/mpld3.v0.1.js")
+MPLD3_URL = "http://mpld3.github.io/js/mpld3.v0.1.js
 
 D3_LOCAL = os.path.join(__path__[0], "js", "d3.v3.min.js")
 
