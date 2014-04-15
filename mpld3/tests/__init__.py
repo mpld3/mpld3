0b58a7f5f4ce1e2990e719281744bb35eecb23d3
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-04-15 06:26:58 -0700

    TST: add figure/axes basic tests

---
+++ mpld3/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+mpld3 tests
+"""
+
+# import matplotlib and set the backend to Agg. This needs to be done before
+# pyplot is imported: we set it here so that we don't have to set it in any
+# of the individual test files.
+import matplotlib
+matplotlib.use('Agg')
