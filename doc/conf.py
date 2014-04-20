efd150903bdb9954c21c8f32bfc784423cfb5de0
Jake Vanderplas <vanderplas@astro.washington.edu>
2014-04-19 20:51:50 -0700

    DOC: add logo and favicon by @dloss

--- doc/conf.py
+++ doc/conf.py
@@ -135,7 +135,7 @@ html_title = "mpld3: Bringing Matplotlib to the Browser"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'logos/mpld3-85.png'
+html_logo = 'logos/mpld3-medium.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
