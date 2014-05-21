5f91fcc1c1824ed2111458d1c6ada1b558e8cf5e
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2014-05-21 15:29:27 +0200

    pep8 related changes to test_twinx.py

--- mpld3/test_plots/test_twinx.py
+++ mpld3/test_plots/test_twinx.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mpld3
 
+
 def create_plot():
     fig = plt.figure()
-    ax1 = fig.add_subplot(1,1,1)
-    
+    ax1 = fig.add_subplot(1, 1, 1)
+
     t = np.arange(0.01, 10.0, 0.01)
     s1 = np.exp(t)
     ax1.plot(t, s1, 'b-')
@@ -15,7 +16,7 @@ def create_plot():
     ax1.set_ylabel('original data', color='b')
     for tl in ax1.get_yticklabels():
         tl.set_color('b')
-    
+
     ax2 = ax1.twinx()
     s2 = np.sin(2*np.pi*t)
     ax2.plot(t, s2, 'r.')
@@ -23,7 +24,7 @@ def create_plot():
     ax2.xaxis.tick_top()
     for tl in ax2.get_yticklabels():
         tl.set_color('r')
-    
+
     return fig
 
 
@@ -31,6 +32,7 @@ def test_twinx():
     fig = create_plot()
     html = mpld3.fig_to_html(fig)
     plt.close(fig)
+
 
 if __name__ == "__main__":
     mpld3.show(create_plot())
