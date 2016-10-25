c21a18f31e0f0b98d854131126dd804529028c58
Eric Galloway <ericgalloway@gmail.com>
2016-10-25 07:53:04 -0500

    Create tick format function

--- mpld3/js/mpld3.v0.3git.js
+++ mpld3/js/mpld3.v0.3git.js
@@ -328,11 +328,6 @@
   };
   mpld3_Axis.prototype.draw = function() {
     var scale = this.props.xy === "x" ? this.parent.props.xscale : this.parent.props.yscale;
-    if (scale === "linear" && this.props.tickvalues && this.props.tickformat) {
-      tick_labels = d3.scale.threshold().domain(this.props.tickvalues.slice(1)).range(this.props.tickformat);
-    } else {
-      tick_labels = null;
-    }
     if (scale === "date" && this.props.tickvalues) {
       var domain = this.props.xy === "x" ? this.parent.x.domain() : this.parent.y.domain();
       var range = this.props.xy === "x" ? this.parent.xdom.domain() : this.parent.ydom.domain();
@@ -340,16 +335,9 @@
       this.props.tickvalues = this.props.tickvalues.map(function(value) {
         return new Date(ordinal_to_js_date(value));
       });
-      if (this.props.tickformat === null) {
-        var tick_labels = null;
-      } else {
-        var labels = this.props.tickformat;
-        tick_labels = function(d, i) {
-          return labels[i];
-        };
-      }
     }
-    this.axis = d3.svg.axis().scale(this.scale).orient(this.props.position).ticks(this.props.nticks).tickValues(this.props.tickvalues).tickFormat(tick_labels);
+    var tickformat = mpld3_tickFormat(this.props.tickformat, this.props.tickvalues, scale);
+    this.axis = d3.svg.axis().scale(this.scale).orient(this.props.position).ticks(this.props.nticks).tickValues(this.props.tickvalues).tickFormat(tickformat);
     this.filter_ticks(this.axis.tickValues, this.axis.scale().domain());
     this.elem = this.ax.baseaxes.append("g").attr("transform", this.transform).attr("class", this.cssclass).call(this.axis);
     mpld3.insert_css("div#" + this.ax.fig.figid + " ." + this.cssclass + " line, " + " ." + this.cssclass + " path", {
@@ -364,6 +352,24 @@
       stroke: "none"
     });
   };
+  function mpld3_tickFormat(tickformat, tickvalues, scale) {
+    if (scale === "linear" && tickvalues && tickformat) {
+      tick_labels = d3.scale.threshold().domain(tickvalues.slice(1)).range(tickformat);
+    } else {
+      tick_labels = null;
+    }
+    if (scale === "date" && tickvalues) {
+      if (tickformat === null) {
+        var tick_labels = null;
+      } else {
+        var labels = tickformat;
+        tick_labels = function(d, i) {
+          return labels[i];
+        };
+      }
+    }
+    return tick_labels;
+  }
   mpld3_Axis.prototype.zoomed = function() {
     this.filter_ticks(this.axis.tickValues, this.axis.scale().domain());
     this.elem.call(this.axis);
