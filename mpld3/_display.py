57ed37dbc4749259b1b46cba8bf28de802972adb
sboesch <sboesch@intuit.com>
2017-11-19 12:40:42 -0800

    Added fix from https://github.com/mpld3/mpld3/issues/434

--- mpld3/_display.py
+++ mpld3/_display.py
@@ -126,7 +126,6 @@ TEMPLATE_DICT = {"simple": SIMPLE_HTML,
 
 class NumpyEncoder(json.JSONEncoder):
     """ Special json encoder for numpy types """
-
     def default(self, obj):
         if isinstance(obj, (numpy.int_, numpy.intc, numpy.intp, numpy.int8,
             numpy.int16, numpy.int32, numpy.int64, numpy.uint8,
@@ -135,9 +134,10 @@ class NumpyEncoder(json.JSONEncoder):
         elif isinstance(obj, (numpy.float_, numpy.float16, numpy.float32, 
             numpy.float64)):
             return float(obj)
+        elif isinstance(obj,(numpy.ndarray,)): #### This is the fix
+            return obj.tolist()
         return json.JSONEncoder.default(self, obj)
-
-
+    
 def fig_to_dict(fig, **kwargs):
     """Output json-serializable dictionary representation of the figure
 
