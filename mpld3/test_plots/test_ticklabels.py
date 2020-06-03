3baf9ddef37888681962a10a3927db84b979d583
Vlad-Ștefan Harbuz <vlad@vladh.net>
2020-06-03 12:54:13 +0200

    :lipstick:

--- mpld3/test_plots/test_ticklabels.py
+++ mpld3/test_plots/test_ticklabels.py
@@ -6,14 +6,14 @@ import mpld3
 
 def create_plot():
     fig, ax = plt.subplots()
-    ax.plot([2000,2050], [1,2])
+    ax.plot([2000, 2050], [1, 2])
     ax.set_title('Tick label test', size=14)
     return fig
 
 
 def test_date():
     fig = create_plot()
-    html = mpld3.fig_to_html(fig)
+    _ = mpld3.fig_to_html(fig)
     plt.close(fig)
 
 
