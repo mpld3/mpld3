cca86e0e8d417cc855a65abd5e5e7e70bf00cd18
Vlad-Ștefan Harbuz <vlad@vladh.net>
2020-06-03 13:06:29 +0200

    :lipstick:

--- mpld3/test_plots/test_tickformat_str_method.py
+++ mpld3/test_plots/test_tickformat_str_method.py
@@ -6,15 +6,16 @@ from mpld3 import plugins
 
 def create_plot():
     fig, ax = plt.subplots()
-    ax.plot([1,3], [1,2])
+    ax.plot([1, 3], [1, 2])
     fmtr = matplotlib.ticker.StrMethodFormatter("{x:.2f}")
     ax.xaxis.set_major_formatter(fmtr)
     ax.set_title('Tickformat str method test', size=14)
     return fig
 
+
 def test_date():
     fig = create_plot()
-    html = mpld3.fig_to_html(fig)
+    _ = mpld3.fig_to_html(fig)
     plt.close(fig)
 
 
