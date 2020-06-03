fa35b59065a05667c3b7de73f2559e6030ec8dc2
Vlad-Ștefan Harbuz <vlad@vladh.net>
2020-06-03 12:59:39 +0200

    :lipstick:

--- mpld3/test_plots/test_date_ticks.py
+++ mpld3/test_plots/test_date_ticks.py
@@ -3,25 +3,26 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import mpld3
 
+
 def create_plot():
-    times = [datetime(2013, 12, i) for i in range(1,20)]
-    ticks = [times[0],times[1],times[2],times[6],times[-2],times[-1]]
+    times = [datetime(2013, 12, i) for i in range(1, 20)]
+    ticks = [times[0], times[1], times[2], times[6], times[-2], times[-1]]
     labels = [t.strftime("%Y-%m-%d") for t in ticks]
 
     plt.plot_date(times, times, xdate=True, ydate=True)
-    plt.xticks(ticks,labels)
+    plt.xticks(ticks, labels)
     plt.yticks(ticks)
-    plt.xlim([times[1],times[-2]])
-    plt.ylim([times[1],times[-2]])
+    plt.xlim([times[1], times[-2]])
+    plt.ylim([times[1], times[-2]])
 
     return plt.gcf()
 
 
 def test_date():
     fig = create_plot()
-    html = mpld3.fig_to_html(fig)
+    _ = mpld3.fig_to_html(fig)
     plt.close(fig)
 
 
 if __name__ == "__main__":
-    mpld3.show(create_plot(),template_type='simple')
+    mpld3.show(create_plot(), template_type='simple')
