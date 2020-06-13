a074758be6376cc93294e763025006687b96458e
Vlad-Ștefan Harbuz <vlad@vladh.net>
2020-06-14 00:11:27 +0200

    Extract bivariate_normal

--- mpld3/test_plots/test_contourf.py
+++ mpld3/test_plots/test_contourf.py
@@ -5,22 +5,39 @@ import matplotlib.mlab as mlab
 import mpld3
 
 
+def bivariate_normal(X, Y, sigmax=1.0, sigmay=1.0,
+                     mux=0.0, muy=0.0, sigmaxy=0.0):
+    """
+    Bivariate Gaussian distribution for equal shape *X*, *Y*.
+    See `bivariate normal
+    <http://mathworld.wolfram.com/BivariateNormalDistribution.html>`_
+    at mathworld.
+    """
+    Xmu = X-mux
+    Ymu = Y-muy
+
+    rho = sigmaxy/(sigmax*sigmay)
+    z = Xmu**2/sigmax**2 + Ymu**2/sigmay**2 - 2*rho*Xmu*Ymu/(sigmax*sigmay)
+    denom = 2*np.pi*sigmax*sigmay*np.sqrt(1-rho**2)
+    return np.exp(-z/(2*(1-rho**2))) / denom
+
+
 def create_plot():
     x = np.linspace(-3.0, 3.0, 30)
     y = np.linspace(-2.0, 2.0, 30)
     X, Y = np.meshgrid(x, y)
-    Z1 = mlab.bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
-    Z2 = mlab.bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
+    Z1 = bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
+    Z2 = bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
     Z = 10.0 * (Z2 - Z1)
 
     fig, ax = plt.subplots()
-    CS = ax.contourf(X, Y, Z, 30)
+    _ = ax.contourf(X, Y, Z, 30)
     return fig
 
 
 def test_contourf():
     fig = create_plot()
-    html = mpld3.fig_to_html(fig)
+    _ = mpld3.fig_to_html(fig)
     plt.close(fig)
 
 
